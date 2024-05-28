3ff20c1600c90a8b266eee896af77317bd058c7c
dariofusco <fdario@hotmail.it>
2024-05-28 17:12:16 +0200

    importa middleware in app

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const postsRouter = require("./routers/posts.js");
 
+const errors = require("./middlewares/errors.js");
+
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,6 +13,8 @@ app.use('/', postsRouter);
 app.use('/:slug', postsRouter);
 
 app.use('/create', postsRouter);
+
+app.use(errors);
 
 app.listen(port, () => {
     console.log('Server attivo sulla porta http://localhost:3000.');
